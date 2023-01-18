0613fd2ddec4afd3acc9dd5b37575dcadc3994e0
Jacqueline Hopkins <318jlh@gmail.com>
2023-01-18 13:55:50 -0800

    added deleteBoardApi and deleteBoard functionality to app.js

--- src/App.js
+++ src/App.js
@@ -1,217 +1,226 @@
-import './App.css';
-import axios from 'axios';
-import { useEffect, useState } from 'react';
+import "./App.css";
+import axios from "axios";
+import { useEffect, useState } from "react";
 // COMPONENTS
-import Boards from './components/Board';
-import BoardList from './components/BoardList';
-import Card from './components/Card';
-import CardList from './components/CardList';
-import NewBoardForm from './components/NewBoardForm';
-import NewCardForm from './components/NewCardForm';
+import Boards from "./components/Board";
+import BoardList from "./components/BoardList";
+import Card from "./components/Card";
+import CardList from "./components/CardList";
+import NewBoardForm from "./components/NewBoardForm";
+import NewCardForm from "./components/NewCardForm";
 
-
-const kBaseUrl = 'https://oyster-inspiration-board.herokuapp.com/';
+const kBaseUrl = "https://oyster-inspiration-board.herokuapp.com/";
 
 const getAllBoardsApi = () => {
-  return (
-    axios.get(`${kBaseUrl}/boards`)
-      .then(response => {
-        return response.data;
-      })
-      .catch(err => {
-        console.log(err);
-      })
-  );
+  return axios
+    .get(`${kBaseUrl}/boards`)
+    .then((response) => {
+      return response.data;
+    })
+    .catch((err) => {
+      console.log(err);
+    });
 };
 
 const getAllCardsApi = (board_id) => {
-  return axios.get(`${kBaseUrl}/boards/${board_id}/cards`)
-    .then(response => {
-      console.log(response.data)
+  return axios
+    .get(`${kBaseUrl}/boards/${board_id}/cards`)
+    .then((response) => {
+      console.log(response.data);
       return response.data;
     })
-    .catch(err => {
+    .catch((err) => {
       console.log(err);
-    })
+    });
 };
 
 const addNewBoardApi = (title) => {
-  const currentData = { title }
-  return (
-    axios.post(`${kBaseUrl}/boards`, currentData)
-      .then(response => {
-        return response.data;
-      })
-      .catch(err => {
-        console.log(err);
-      })
-  );
+  const currentData = { title };
+  return axios
+    .post(`${kBaseUrl}/boards`, currentData)
+    .then((response) => {
+      return response.data;
+    })
+    .catch((err) => {
+      console.log(err);
+    });
 };
 
 const addNewCardApi = (body, currentBoardID) => {
   const currentData = {
     body,
     likes: 0,
-    board_id: currentBoardID
-  }
+    board_id: currentBoardID,
+  };
 
-
-  return axios.post(`${kBaseUrl}/cards`, currentData)
-    .then(response => {
-
+  return axios
+    .post(`${kBaseUrl}/cards`, currentData)
+    .then((response) => {
       return response.data;
-
     })
-    .catch(err => {
+    .catch((err) => {
       console.log(err);
-    })
-}
-
-const likeCardApi = (card_id) => {
-
-  return axios.patch(`${kBaseUrl}cards/${card_id}/like`)
-    .then(response => {
-      return response.data;
-    })
-    .catch(err => {
-      console.log(err)
     });
-}
-
-const deleteCardApi = (card_id) => {
-  return axios.delete(`${kBaseUrl}/cards/${card_id}`)
-    .then(response => {
-      console.log(response.data)
-    })
-    .catch(err => {
-      console.log(err)
-    })
-
 };
 
+const likeCardApi = (card_id) => {
+  return axios
+    .patch(`${kBaseUrl}cards/${card_id}/like`)
+    .then((response) => {
+      return response.data;
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+};
+
+const deleteCardApi = (card_id) => {
+  return axios
+    .delete(`${kBaseUrl}/cards/${card_id}`)
+    .then((response) => {
+      console.log(response.data);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+};
+
+const deleteBoardApi = (board_id) => {
+  return axios
+    .delete(`${kBaseUrl}/boards/${board_id}`)
+    .then((response) => {
+      console.log(response.data);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+};
 
 function App() {
   const [boardData, setboardData] = useState([]);
   const [cardData, setCardData] = useState([]);
-  const [currentBoardID, setCurrentBoardID] = useState([2]);
+  const [currentBoardID, setCurrentBoardID] = useState([]);
+  const [currentBoardTitle, setCurrentBoardTitle] = useState([]);
   // const [handlelike, sethandleLike] = useState(0)
 
   const getAllBoards = () => {
-    getAllBoardsApi()
-      .then(boards => {
-        setboardData(boards)
-      })
+    getAllBoardsApi().then((boards) => {
+      setboardData(boards);
+    });
   };
 
   const getAllCards = () => {
-    getAllCardsApi()
-      .then(cards => {
-        setCardData(cards)
-      })
+    getAllCardsApi().then((cards) => {
+      setCardData(cards);
+    });
   };
 
   useEffect(() => {
     // data fetching code
 
     getAllBoards();
-
-
   }, [boardData]);
 
-
   const likedCard = (card_id) => {
-    return likeCardApi(card_id)
-      .then(likeResult => {
-        setCardData(cardData => cardData.map(card => {
+    return likeCardApi(card_id).then((likeResult) => {
+      setCardData((cardData) =>
+        cardData.map((card) => {
           if (card.card_id === likeResult.card_id) {
-            return likeResult
+            return likeResult;
           } else {
-            return card
+            return card;
           }
-        }))
-      })
-  }
-
-
-
-  const deleteCard = card_id => {
-    deleteCardApi(card_id)
-    setCardData(cardData => cardData.filter(card => {
-      return card.card_id !== card_id;
-    }));
+        })
+      );
+    });
   };
-  const boardClick = (board_id) => {
 
-    getAllCardsApi(board_id)
-      .then(board => {
-        setCardData(board.cards)
-        setCurrentBoardID(board.board_id)
+  const deleteCard = (card_id) => {
+    deleteCardApi(card_id);
+    setCardData((cardData) =>
+      cardData.filter((card) => {
+        return card.card_id !== card_id;
       })
+    );
+  };
+  // function incomplete, building get board api
+  const deleteBoard = () => {
+    getAllCardsApi(currentBoardID).then((boardData) => {
+      boardData.cards.map((card) => {
+        deleteCard(card.card_id);
+      });
+    });
+    deleteBoardApi(currentBoardID);
+    setCurrentBoardID([]);
+    setCurrentBoardTitle([]);
+  };
+
+  const boardClick = (board_id) => {
+    getAllCardsApi(board_id).then((board) => {
+      setCardData(board.cards);
+      setCurrentBoardID(board.board_id);
+      setCurrentBoardTitle(board.title);
+    });
   };
 
   const handleBoardSubmit = (data) => {
     addNewBoardApi(data)
-      .then(newBoard => {
-        setboardData([...boardData, newBoard])
-
-
+      .then((newBoard) => {
+        setboardData([...boardData, newBoard]);
       })
-      .catch(e => console.log(e));
+      .catch((e) => console.log(e));
   };
 
   const handleCardSubmit = (newCardMessage, currentBoardID) => {
     addNewCardApi(newCardMessage, currentBoardID)
+      .then((newCard) => {
+        console.log(newCard);
 
-      .then(newCard => {
+        setCardData([...cardData, newCard]);
 
-        console.log(newCard)
-
-        setCardData([...cardData, newCard])
-
-
-        //  response is currently - card # created 
+        //  response is currently - card # created
       })
-      .catch(e => console.log(e))
-  }
+      .catch((e) => console.log(e));
+  };
 
   return (
     <div className="App">
       <header className="App-header">Inspiration Board</header>
       <h1>Boards</h1>
 
-      <div className='boxed'>
-        <BoardList boardData={boardData} handleBoardSubmit={handleBoardSubmit} onboardClick={boardClick} handleCardSubmit={handleCardSubmit} ></BoardList>
+      <div className="boxed">
+        <BoardList
+          boardData={boardData}
+          handleBoardSubmit={handleBoardSubmit}
+          onboardClick={boardClick}
+          handleCardSubmit={handleCardSubmit}
+        ></BoardList>
       </div>
 
-      <h1>Current Board: {currentBoardID}</h1>
+      <h1>
+        {currentBoardID}. {currentBoardTitle}
+        <button onClick={() => deleteBoard()}>Delete Board</button>
+      </h1>
 
       <h1>Create a New Board</h1>
-      <NewBoardForm handleBoardSubmit={handleBoardSubmit} ></NewBoardForm>
+      <NewBoardForm handleBoardSubmit={handleBoardSubmit}></NewBoardForm>
 
+      <CardList
+        onlikedCard={likedCard}
+        cardData={cardData}
+        ondeleteCard={deleteCard}
+        handleCardSubmit={handleCardSubmit}
+      />
 
-      <CardList onlikedCard={likedCard} cardData={cardData} ondeleteCard={deleteCard} handleCardSubmit={handleCardSubmit} />
-
-      <NewCardForm handleCardSubmit={handleCardSubmit} currentBoardID={currentBoardID} />
-
-
-
-
+      <NewCardForm
+        handleCardSubmit={handleCardSubmit}
+        currentBoardID={currentBoardID}
+      />
     </div>
-  )
+  );
 }
 
-
-
-
-
-
-
 export default App;
-
-
-
-
-
 
 // <img src={logo} className="App-logo" alt="logo" />
 //         <p>
