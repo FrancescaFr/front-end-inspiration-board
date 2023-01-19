02684094dacd4a58cb3ac9c6ab98780e410266df
Jacqueline Hopkins <318jlh@gmail.com>
2023-01-19 09:05:49 -0800

    added classname and imports

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,38 +1,36 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import { useState } from 'react';
-import NewCardForm from './NewCardForm';
-
+import React from "react";
+import PropTypes from "prop-types";
+import { useState } from "react";
+import NewCardForm from "./NewCardForm";
+import "./Card.css";
 
 const Card = (props) => {
-  
-  return(
-  
-   <div>
-    
-  <div><section></section></div>
-  
+  return (
+    <div className="individual-card">
+      <div>
+        <section></section>
+      </div>
 
-  
-  <h3 className="entry-name">{props.body}</h3>
-  <section className="entry-bubble">
-    <button className='num_likes'> {props.likes} 💕</button>
-    <button className='delete_card' onClick={() => props.ondeleteCard(props.card_id)} >Delete</button>
-    <button onClick={() => props.onlikedCard(props.card_id)}>+1</button>
-    </section>
-  
-  </div>
-  
-
-
-)
+      <h3 className="entry-name">{props.body}</h3>
+      <section className="entry-bubble">
+        <button className="num_likes"> {props.likes} 💕</button>
+        <button
+          className="delete_card"
+          onClick={() => props.ondeleteCard(props.card_id)}
+        >
+          Delete
+        </button>
+        <button onClick={() => props.onlikedCard(props.card_id)}>+1</button>
+      </section>
+    </div>
+  );
 };
-<NewCardForm/>
+<NewCardForm />;
 Card.propTypes = {
   likes: PropTypes.number.isRequired,
   body: PropTypes.string.isRequired,
   // board_id: PropTypes.number.isRequired,
   card_id: PropTypes.number.isRequired,
-}
+};
 
 export default Card;
